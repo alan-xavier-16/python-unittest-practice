28f9fce4da14db275e5b6e10c47cb720e8c698a8
Alan Xavier <alan.xavier@bmw.de>
2021-11-09 13:57:13 +0100

    Add 2nd test case

--- test_sum.py
+++ test_sum.py
@@ -1,6 +1,10 @@
-def test_sum():
+def test_sum_list():
     assert sum([1,2,3])==6, "Should be 6"
 
+def test_sum_tuple():
+    assert sum((1,2,3))==6, "Should be 6"
+
 if __name__ == "__main__":
-    test_sum()
+    test_sum_list()
+    test_sum_tuple()
     print("Sum Test passed")
